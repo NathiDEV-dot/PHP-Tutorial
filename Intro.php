dfe2dad5ac482b6dc882d802caa8f21c3fd02a9b
Nkosinathi Tshabalala <nkosinathit217@gmail.com>
2025-07-22 09:25:29 +0200

    Variables and output

--- Intro.php
+++ Intro.php
@@ -4,4 +4,24 @@ echo "Wassup"; //  PHP statements end with a semicolon (;).
 //In PHP, keywords (e.g. if, else, while, echo, etc.), classes, functions, and user-defined functions are not case-sensitive.
 Echo "Hello, world!";
 EcHo "Welcome to your first lesson";
+
+/*
+A variable starts with the $ sign, followed by the name of the variable
+A variable name must start with a letter or the underscore character
+A variable name cannot start with a number
+A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+Variable names are case-sensitive ($age and $AGE are two different variables)
+*/
+
+$age = 22;
+$name = "John Doe";
+$price = 14.67;
+$is_valid = true;
+
+// The following example will show how to output text and a variable:
+
+echo "My name is $name" . "\n";
+echo "I am $age years old" . "\n";
+echo "This item costs R$price" . "\n";
+echo "The answwer is $is_valid" . "\n";
 ?>
