225a077949d6b461c308890dc6d4fb2609a27ccb
Nkosinathi Tshabalala <nkosinathit217@gmail.com>
2025-07-22 09:45:02 +0200

    Data types

--- Intro.php
+++ Intro.php
@@ -24,4 +24,29 @@ echo "My name is $name" . "\n";
 echo "I am $age years old" . "\n";
 echo "This item costs R$price" . "\n";
 echo "The answwer is $is_valid" . "\n";
+
+// PHP Data Types
+// String - A string is a sequence of characters, like "Hello world!".
+$text = "Yeah buddy";
+
+// Integer - An integer data type is a non-decimal number between -2,147,483,648 and 2,147,483,647.
+$count = 0;
+
+//Float (floating point numbers - also called double) - A float (floating point number) is a number with a decimal point or a number in exponential form.
+$temp = 67.83;
+
+// Boolean - A Boolean represents two possible states: TRUE or FALSE.
+$is_member = false;
+
+// Array - An array stores multiple values in one single variable.
+$scores = array(22, 14, 67, 11);
+$names = array("nathi", "katlego", "marlon");
+
+/*
+Null is a special data type which can have only one value: NULL.
+A variable of data type NULL is a variable that has no value assigned to it.
+Tip: If a variable is created without a value, it is automatically assigned a value of NULL.
+Variables can also be emptied by setting the value to NULL:
+*/
+$x = null;
 ?>
