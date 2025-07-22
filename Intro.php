43a578a72962ab08679b6188fc9a7a388421c68e
Nkosinathi Tshabalala <nkosinathit217@gmail.com>
2025-07-22 09:08:48 +0200

    Comments and case sensitivity lesson

--- Intro.php
+++ Intro.php
@@ -1,3 +1,7 @@
 <?php
-echo "Wassup";
+echo "Wassup"; //  PHP statements end with a semicolon (;).
+
+//In PHP, keywords (e.g. if, else, while, echo, etc.), classes, functions, and user-defined functions are not case-sensitive.
+Echo "Hello, world!";
+EcHo "Welcome to your first lesson";
 ?>
